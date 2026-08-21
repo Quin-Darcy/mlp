40e4fc0d305e8cac4311ff7a0bd6dbd2d31b8666
quin <quin.darcy@atsec.com>
2026-08-21 18:28:00 -0500

    Finish first complete unit test for network forward pass

--- src/network.rs
+++ src/network.rs
@@ -43,7 +43,7 @@ impl Network {
         let mut cache = ForwardCache::new();
 
         for layer in &self.layers {
-            layer_output = layer.forward_pass(&layer_input)?;
+            layer_output = layer.forward_pass(&layer_input)?; // match to prop error type?
             cache.entries.push(Entry {
                 input: layer_input.clone(),
                 pre_activation: layer_output.pre_activation,
@@ -123,35 +123,39 @@ mod tests {
         let test_activation3 = Activation::RELU;
         let test_layer3 = Layer::new(test_weights3, test_biases3, test_activation3).unwrap();
 
-        let layers: Vec<Layer> = vec![test_layer1, test_layer2, test_layer3];
-        let result = Network::new(layers);
+        let test_layers: Vec<Layer> = vec![test_layer1, test_layer2, test_layer3];
+        let result = Network::new(test_layers);
         assert!(result.is_err());
     }
 
     #[test]
     fn test_network_forward_pass() {
-       let test_weights1: Array2<f32> = array![
+        // Test 1
+        let test_weights11: Array2<f32> = array![
             [1.0, 0.0, 0.5, 0.5],
             [0.0, 1.0, 0.5, -0.5]
-       ];
-       let test_biases1: Array1<f32> = array![0.0, 0.0];
-       let test_activation1 = Activation::RELU;
-       let test_layer1 = Layer::new(test_weights1, test_biases1, test_activation1).unwrap();
+        ];
+        let test_biases11: Array1<f32> = array![0.0, 0.0];
+        let test_activation11 = Activation::RELU;
+        let test_layer11 = Layer::new(test_weights11, test_biases11, test_activation11).unwrap();
 
-       let test_weights2: Array2<f32> = array![
+        let test_weights12: Array2<f32> = array![
             [1.0, 0.0],
             [0.0, 1.0],
             [0.5, 0.5],
             [0.5, -0.5]
-       ];
-       let test_biases2: Array1<f32> = array![0.0, 0.0, 0.0, 0.0];
-       let test_activation2 = Activation::RELU;
-       let test_layer2 = Layer::new(test_weights2, test_biases2, test_activation2).unwrap();
+        ];
+        let test_biases12: Array1<f32> = array![0.0, 0.0, 0.0, 0.0];
+        let test_activation12 = Activation::RELU;
+        let test_layer12 = Layer::new(test_weights12, test_biases12, test_activation12).unwrap();
 
-       let layers: Vec<Layer> = vec![test_layer1, test_layer2];
-       let test_network = Network::new(layers).unwrap();
+        let test_layers1: Vec<Layer> = vec![test_layer11, test_layer12];
+        let test_network1 = Network::new(test_layers1).unwrap();
 
-       let test_input: Array1<f32> = array![0.0, 0.0, 0.0, 1.0];
-       let result = test_network.forward_pass(&test_input);
+        let test_input1: Array1<f32> = array![0.0, 0.0, 0.0, 1.0];
+        let test_expected_output1: Array1<f32> = array![0.5, 0.0, 0.25, 0.25];
+        let result1 = test_network1.forward_pass(&test_input1).unwrap();
+
+        assert_eq!(result1.output, test_expected_output1);
     }
 }
