63290869c8195a8b483cf6babbfeb531c04b0840
quin <quin.darcy@atsec.com>
2026-08-21 12:01:05 -0500

    Update network unit tests

--- src/network.rs
+++ src/network.rs
@@ -33,7 +33,6 @@ impl Network {
     #[must_use]
     pub fn forward_pass(&self, input: &Array1<f32>) -> Result<ForwardCache, LayerError> {
         let mut layer_input: Array1<f32> = input.clone();
-        let _pre_activations: Array1<f32>;
         let mut cache = ForwardCache::new();
 
         for layer in &self.layers {
@@ -115,9 +114,34 @@ mod tests {
         let test_layer3 = Layer::new(test_weights3, test_biases3, test_activation3).unwrap();
 
         let layers: Vec<Layer> = vec![test_layer1, test_layer2, test_layer3];
-        
         let result = Network::new(layers);
         assert!(result.is_err());
     }
 
+    #[test]
+    fn test_network_forward_pass() {
+       let test_weights1: Array2<f32> = array![
+            [1.0, 0.0, 0.5, 0.5],
+            [0.0, 1.0, 0.5, -0.5]
+       ];
+       let test_biases1: Array1<f32> = array![0.0, 0.0];
+       let test_activation1 = Activation::RELU;
+       let test_layer1 = Layer::new(test_weights1, test_biases1, test_activation1).unwrap();
+
+       let test_weights2: Array2<f32> = array![
+            [1.0, 0.0],
+            [0.0, 1.0],
+            [0.5, 0.5],
+            [0.5, -0.5]
+       ];
+       let test_biases2: Array1<f32> = array![0.0, 0.0, 0.0, 0.0];
+       let test_activation2 = Activation::RELU;
+       let test_layer2 = Layer::new(test_weights2, test_biases2, test_activation2).unwrap();
+
+       let layers: Vec<Layer> = vec![test_layer1, test_layer2];
+       let test_network = Network::new(layers).unwrap();
+
+       let test_input: Array1<f32> = array![0.0, 0.0, 0.0, 1.0];
+       let result = test_network.forward_pass(&test_input);
+    }
 }
