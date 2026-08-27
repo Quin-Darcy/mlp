f314646486cb153c8f0c3c4084c3062111bab401
quin <quin.darcy@atsec.com>
2026-08-27 16:15:36 -0500

    Add gradient method and unit tests

--- src/objective.rs
+++ src/objective.rs
@@ -11,20 +11,36 @@ pub enum Objective {
 }
 
 impl Objective {
-    pub fn compute(&self, output: &Array1<f32>, target: &Array1<f32>) -> Result<f32, ObjectiveError> {
-        if output.dim() != target.dim() {
+    pub fn compute(&self, input: &Array1<f32>, target: &Array1<f32>) -> Result<f32, ObjectiveError> {
+        if input.dim() != target.dim() {
             return Err(ObjectiveError::InvalidArgDimensions(
-                "output and target vectors must be the same length".to_string()
+                "input and target vectors must be the same length".to_string()
             ));
         }
 
         match self {
             Self::MSE => {
-                let diff = target - output;
+                let diff: Array1<f32> = target - input;
                 Ok(diff.dot(&diff) / diff.len() as f32)
             }
         }
     }
+
+    pub fn gradient(&self, input: &Array1<f32>, target: &Array1<f32>) -> Result<Array1<f32>, ObjectiveError> {
+        if input.dim() != target.dim() {
+            return Err(ObjectiveError::InvalidArgDimensions(
+                "input and target vectors must be the same length".to_string()
+            ));
+        }
+
+        match self {
+            Self::MSE => {
+                let scalar: f32 = -2.0 / (input.dim() as f32);
+                let diff: Array1<f32> = target - input;
+                Ok(scalar * diff)
+            }
+        }
+    } 
 }
 
 #[cfg(test)]
@@ -33,33 +49,65 @@ mod tests {
     use ndarray::array;
 
     #[test]
-    fn test_objective_mse_valid_args() {
-        let test_output: Array1<f32> = array![1.0, 0.0];
+    fn test_objective_mse_compute_valid_args() {
+        let test_input: Array1<f32> = array![1.0, 0.0];
         let test_target: Array1<f32> = array![1.0, 0.0];
         let test_objective = Objective::MSE;
-        let result = test_objective.compute(&test_output, &test_target);
+        let result = test_objective.compute(&test_input, &test_target);
 
         assert!(result.is_ok());
     }
 
     #[test]
-    fn test_objective_mse_invalid_args() {
-        let test_output: Array1<f32> = array![1.0, 0.0, 0.0];
+    fn test_objective_mse_compute_invalid_args() {
+        let test_input: Array1<f32> = array![1.0, 0.0, 0.0];
         let test_target: Array1<f32> = array![1.0, 0.0];
         let test_objective = Objective::MSE;
-        let result = test_objective.compute(&test_output, &test_target);
+        let result = test_objective.compute(&test_input, &test_target);
 
         assert!(result.is_err());
     }
 
     #[test]
-    fn test_objective_mse() {
-        let test_output: Array1<f32> = array![1.0, 2.0];
+    fn test_objective_mse_compute() {
+        let test_input: Array1<f32> = array![1.0, 2.0];
         let test_target: Array1<f32> = array![3.0, 0.0];
         let test_objective = Objective::MSE;
         let test_expected_value: f32 = 4.0;
-        let test_value: f32 = test_objective.compute(&test_output, &test_target).unwrap();
+        let test_value: f32 = test_objective.compute(&test_input, &test_target).unwrap();
 
         assert_eq!(test_expected_value, test_value);
     }
+
+    #[test]
+    fn test_objective_mse_gradient_valid_args() {
+        let test_input: Array1<f32> = array![1.0, 0.0];
+        let test_target: Array1<f32> = array![1.0, 0.0];
+        let test_objective = Objective::MSE;
+        let result = test_objective.gradient(&test_input, &test_target);
+
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_objective_mse_gradient_invalid_args() {
+        let test_input: Array1<f32> = array![1.0, 0.0, 0.0];
+        let test_target: Array1<f32> = array![1.0, 0.0];
+        let test_objective = Objective::MSE;
+        let result = test_objective.gradient(&test_input, &test_target);
+
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_objective_mse_gradient() {
+        let test_input: Array1<f32> = array![1.0, 2.0];
+        let test_target: Array1<f32> = array![3.0, 0.0];
+        let test_objective = Objective::MSE;
+        let test_expected_value: Array1<f32> = array![-2.0, 2.0];
+        let test_value: Array1<f32> = test_objective.gradient(&test_input, &test_target).unwrap();
+
+        assert_eq!(test_expected_value, test_value);
+    }
+
 }
