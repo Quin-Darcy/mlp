3fe65944f82f7646b49cadcb32523df9fe760c6a
quin <quin.darcy@atsec.com>
2026-08-27 15:46:14 -0500

    Add cache struct for back prop

--- src/cache.rs
+++ src/cache.rs
@@ -1,9 +1,15 @@
-use ndarray::Array1;
+use ndarray::{Array1, Array2};
 
 #[derive(Debug, PartialEq)]
 pub struct ForwardEntry {
     pub input: Array1<f32>,
     pub pre_activation: Array1<f32>,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct BackwardEntry {
+    pub bias_gradient: Array1<f32>,
+    pub weight_gradient: Array2<f32>,
 }
 
 #[derive(Default, Debug, PartialEq)]
@@ -12,6 +18,25 @@ pub struct ForwardCache {
 }
 
 impl ForwardCache {
+    #[must_use]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    #[must_use]
+    pub fn with_capacity(num_layers: usize) -> Self {
+        Self {
+            entries: Vec::with_capacity(num_layers),
+        }
+    }
+}
+
+#[derive(Default, Debug, PartialEq)]
+pub struct BackwardCache {
+    pub entries: Vec<BackwardEntry>,
+}
+
+impl BackwardCache {
     #[must_use]
     pub fn new() -> Self {
         Self::default()
