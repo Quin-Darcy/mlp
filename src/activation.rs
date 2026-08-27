0b2d061d0bd3931b2d50c405ef61bf8f932ea3dd
quin <quin.darcy@atsec.com>
2026-08-27 18:48:43 -0500

    Add jacobian methods for activation functions

--- src/activation.rs
+++ src/activation.rs
@@ -1,4 +1,4 @@
-use ndarray::Array1;
+use ndarray::{Array1, Array2};
 
 pub enum Activation {
     RELU,
@@ -12,6 +12,28 @@ impl Activation {
             Self::RELU => pre_activation.mapv(|x| x.max(0.0)),
             Self::IDENTITY => pre_activation.clone(),
         }
+    }
+
+    pub fn jacobian(&self, pre_activation: &Array1<f32>) -> Array1<f32> {
+        /* RELU and the IDENTITY act component wise and so the jacobian
+         * is always a diagnonal matrix. That is why we only return a row
+         * vector and we will replace the matrix multiplication with a 
+         * Hadamard product to get the same result, just more efficiently
+         */
+        let dim: usize = pre_activation.dim();
+        let mut jacobian: Array1<f32> = Array1::ones(dim);
+
+        match self {
+            Self::RELU => {
+                for i in 0..dim {
+                    if pre_activation[i] < 0.0 {
+                        jacobian[i] = 0.0;
+                    }
+                }
+            },
+            Self::IDENTITY => {}
+        }
+        jacobian
     }
 }
 
@@ -42,5 +64,25 @@ mod tests {
         let test_input: Array1<f32> = array![23.4, 0.5, -9.9];
         let test_expected: Array1<f32> = test_input.clone();
         assert_eq!(test_activation.apply(&test_input), test_expected);
+    }
+
+    #[test]
+    fn test_activation_relu_jacobian() {
+        let test_activation = Activation::RELU;
+
+        let test_pre_activation: Array1<f32> = array![2.0, -1.0, -0.1, 0.1, 0.0];
+        let test_expected_result: Array1<f32> = array![1.0, 0.0, 0.0, 1.0, 1.0];
+
+        assert_eq!(test_activation.jacobian(&test_pre_activation), test_expected_result);
+    }
+
+    #[test]
+    fn test_activation_identity_jacobian() {
+        let test_activation = Activation::IDENTITY;
+
+        let test_pre_activation: Array1<f32> = array![2.0, -1.0, -0.1, 0.1, 0.0];
+        let test_expected_result: Array1<f32> = array![1.0, 1.0, 1.0, 1.0, 1.0];
+
+        assert_eq!(test_activation.jacobian(&test_pre_activation), test_expected_result);
     }
 }
