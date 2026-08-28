f1cac13ad6ab8dcea52d839fc5b2c40272b79703
quin <quin.darcy@atsec.com>
2026-08-28 14:01:04 -0500

    Empty out main for now

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,1 @@
-use rand::SeedableRng;
-use rand::rngs::StdRng;
-
-use mlp::activation::Activation;
-use mlp::layer::Layer;
-
-fn main() {
-    let seed: u64 = 48;
-    let mut rng = StdRng::seed_from_u64(seed);
-
-    let test_dims: [usize; 2] = [2, 4];
-    let test_range: [f32; 2] = [-1.0, 1.0];
-    let test_activation = Activation::RELU;
-
-    let test_layer = Layer::new_random(test_dims, test_range, test_activation, &mut rng).unwrap();
-    println!("Biases: {:?}", test_layer.biases);
-    println!("Weights: {:?}", test_layer.weights);
-}
+fn main() {}
