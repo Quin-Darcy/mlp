daef85f1519b6aa7aed80f7c34fad38533ecbc19
quin <quin.darcy@atsec.com>
2026-08-29 14:02:31 -0500

    Add error types and new method for DataSet

--- src/data_set.rs
+++ src/data_set.rs
@@ -1,5 +1,12 @@
 use ndarray::Array1;
 
+#[derive(Debug)]
+pub enum DataError {
+    EmptySamples(String),
+    EmptyLabels(String),
+    InvalidSizes(String),
+}
+
 #[derive(Default)]
 pub struct DataSet {
     pub samples: Vec<Array1<f32>>,
@@ -11,6 +18,28 @@ impl DataSet {
     pub fn new() -> Self {
         Self::default()
     }
-}
 
-// TODO: add error type and validate samples and labels are same size
+    #[must_use]
+    pub fn from_data(
+        samples: Vec<Array1<f32>>,
+        labels: Vec<Array1<f32>>,
+    ) -> Result<Self, DataError> {
+        if samples.is_empty() {
+            return Err(DataError::EmptySamples(
+                "Samples cannot be empty".to_string(),
+            ));
+        }
+
+        if labels.is_empty() {
+            return Err(DataError::EmptyLabels("Labels cannot be empty".to_string()));
+        }
+
+        if samples.len() != labels.len() {
+            return Err(DataError::InvalidSizes(
+                "There must be an equal number of samples and labels".to_string(),
+            ));
+        }
+
+        Ok(Self { samples, labels })
+    }
+}
